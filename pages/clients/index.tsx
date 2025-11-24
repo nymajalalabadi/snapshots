e06b434f2127f3d42023b34d7ff1fecd4b39952f
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-24 08:53:42 -0600

    Updated ClientsPage

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
   return (
-    <div>
-        <h1>Clients</h1>
-        <p>Your clients</p>
+    <div className="p-4">
+        <h1 className="text-2xl font-bold">Clients</h1>
+        <p className="text-gray-500">Your clients</p>
+        <ul className="flex gap-2">
+            <li className="bg-blue-500 text-white p-2 rounded-md">
+                <Link href="/clients/Maxwell" className="text-white">Maxwell</Link>
+            </li>
+            <li className="bg-blue-500 text-white p-2 rounded-md">
+                <Link href="/clients/John" className="text-white">John</Link>
+            </li>
+            <li className="bg-blue-500 text-white p-2 rounded-md">
+                <Link href="/clients/Jane" className="text-white">Jane</Link>
+            </li>
+        </ul>
     </div>
   );
 }
