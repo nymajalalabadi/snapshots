093d7d586ad0e8341c20774cc8268490878454d4
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-24 08:52:11 -0600

    Updated Index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div>
-        <h1>Snapshots</h1>
-        <p>Take snapshots of your life</p>
+    <div className="p-4">
+        <h1 className="text-2xl font-bold">Snapshots</h1>
+        <p className="text-gray-500">Take snapshots of your life</p>
+        <ul className="flex gap-2">
+          <li className="bg-blue-500 text-white p-2 rounded-md">
+            <Link href="/portfolio" className="text-white">Portfolio</Link>
+          </li>
+          <li className="bg-blue-500 text-white p-2 rounded-md">
+            <Link href="/blog" className="text-white">Blog</Link>
+          </li>
+          <li className="bg-blue-500 text-white p-2 rounded-md">
+            <Link href="/clients" className="text-white">Clients</Link>
+          </li>
+        </ul>
     </div>
     );
 }
