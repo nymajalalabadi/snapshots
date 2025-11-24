ea035c0acbf9aba5865835eb765f07cfd845a47c
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-24 08:05:10 -0600

    Added useRouter

--- pages/portfolio/[projectid].tsx
+++ pages/portfolio/[projectid].tsx
@@ -1,8 +1,14 @@
+import { useRouter } from 'next/router';
+
 export default function PortfolioProjectPage() {
+  const router = useRouter();
+  const projectId = router.query.projectid;
+  
   return (
     <div>
         <h1>Portfolio Project</h1>
         <p>Your project of snapshots</p>
+        <p>Project ID: {projectId}</p>
     </div>
   );
 }
